4ee328836f65a6af54170b332c0e8ca6ad60bc66
austin sanchez <austinsanchez45@yahoo.com>
2021-11-19 14:10:27 -0600

    Added delete functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
-import { useState } from 'react';
+import DisplaySongs from ",/component_songs/DisplaySongs";
 
 
 const App = () => {
   // const[MusicTable, setMusicTable] = useState(data);
   const getAllSongs = async() => {
     await axios
-    .get(`https://jsonplaceholder.typicode.com/users`)
+    .get("http://127.0.0.1:8000/musicapp/")
     .then((res) => {
         console.log(res.data);
         const people = res.data;
         this.setState({ people });
     }); 
+  }
+
+  deleteSongs = async (songs) => {
+    let response = await axios.delete("http://127.0.0.1:8000/musicapp/");
+  }
+
+  handleSubmit = (event)=> {
+    event.preventDefault[{}];
+    const form_data = {
+      title: this.state.title,
+      artist: this.state.artist,
+      album: this.state.album,
+      release_date: this.state.release_date,
+      genre: this.state.genre,
+    };
+    let response = axios.post("http://127.0.0.1:8000/musicapp/", form_data);
+    let data = response.data;
   }
 
   return (
